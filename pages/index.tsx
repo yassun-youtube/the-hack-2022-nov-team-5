ffb143b5de44f5f47493495fefaadbb326065c64
takeshi_honda <honda0924@hotmail.co.jp>
2022-11-27 01:06:08 +0900

    feat: update README, format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '../lib/firebase'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../lib/firebase';
 
 const Home = () => {
-  const [postList, setPostList] = useState([])
+  const [postList, setPostList] = useState([]);
   useEffect(() => {
     const getPosts = async () => {
-      const data = await getDocs(collection(db, "tests"))
-      console.log(process.env.FIREBASE_API_KEY)
-      setPostList(data.docs.map((doc)=>({...doc.data(), id: doc.id})))
-    }
-    getPosts()
-  }, [])
+      const data = await getDocs(collection(db, 'tests'));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
   return (
-    <div className='homePage'>
+    <div className="homePage">
       {postList.map((post) => {
         return (
-          <div className='postContents' key={post.id}>
+          <div className="postContents" key={post.id}>
             {post.context}
           </div>
-        )
+        );
       })}
-
     </div>
-  )
-}
+  );
+};
 
 export default Home;
