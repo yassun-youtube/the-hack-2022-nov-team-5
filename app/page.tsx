96322faa12e13804a351c8da48de6c326e4ba03f
KenIshida <kenishida95@gmail.com>
2022-12-14 18:35:09 +0900

    feat: create header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../lib/firebase';
+import Header from './components/header'
 
 const Home = () => {
-  const [postList, setPostList] = useState([]);
-  useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(collection(db, 'tests'));
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getPosts();
-  }, []);
   return (
-    <div className="homePage">
-      {postList.map((post) => {
-        return (
-          <div className="postContents font-bold underline" key={post.id}>
-            {post.context}
-          </div>
-        );
-      })}
-    </div>
-  );
+    <Header />
+  )
 };
 
 export default Home;
